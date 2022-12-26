93f09d798e125ace0ffe51b2d9c5dde6c1c80d09
IkbalKulahcioglu <ikbalkulahcioglu@gmail.com>
2022-12-26 23:37:21 +0300

    Table added

--- src/app/tables/tables/tables.component.ts
+++ src/app/tables/tables/tables.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent {
-
+data =[
+  {name:'James', age:24, job:'Engineer'},
+  {name:'Jill', age:26, job:'Engineer'},
+  {name:'Elyse', age:24, job:'Designer'}
+];
+headers=[
+  {key:'name',label:'Name'},
+  {key:'age',label:'Age'},
+  {key:'job',label:'Job'}
+]
 }
